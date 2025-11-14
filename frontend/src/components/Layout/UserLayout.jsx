3ecf9e84987b5f3380f83ce9ec40d3cc807f15eb
Pallab Mallick <ipallabmallick@gmail.com>
2025-11-14 21:04:45 +0600

    Change

--- frontend/src/components/Layout/UserLayout.jsx
+++ frontend/src/components/Layout/UserLayout.jsx
@@ -1,10 +1,7 @@
 import React from "react";
 import Header from "./../Common/Header";
-<<<<<<< HEAD
 import Footer from "./../Common/Footer";
-=======
 import { Outlet } from "react-router";
->>>>>>> Herosection/Ewebapp
 
 const UserLayout = () => {
   return (
