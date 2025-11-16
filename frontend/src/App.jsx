a151ad4ba6ddf23d0e34718c443703b7cf1bc470
aman <git>
2025-11-16 18:49:02 +0600

    update dom

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router";
 import UserLayout from "./components/Layout/UserLayout";
 import Home from "./pages/Home";
 
