a90456450aec566b5b83ac2a86baf9bad0d594c3
Pallab Mallick <ipallabmallick@gmail.com>
2025-11-16 11:07:49 +0600

    add Newarrivals

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Hero from './../components/Layout/Hero';
 import GenderCollectionSection from '../components/Products/GenderCollectionSection';
+import Newarrivals from './../components/Products/Newarrivals';
 
 const Home = () => {
   return (
     <div>
       <Hero />
       <GenderCollectionSection />
+      <Newarrivals />
     </div>
   );
 };
