ae138ce9b5128fa7b53fde6c40d3750f66d09d0c
Pallab Mallick <ipallabmallick@gmail.com>
2025-11-18 12:38:51 +0600

    Add price

--- frontend/src/components/Products/ProductGrid.jsx
+++ frontend/src/components/Products/ProductGrid.jsx
@@ -10,9 +10,10 @@ const ProductGrid = ({ products }) => {
                  
                  <div className="w-full h-96 mb-4">
                     <img src={product.images[0].url} alt={product.images[0].alText || product.name} className="w-full h-full object-cover rounded-lg" />
-
-
                  </div>
+                 <h3 className="text-sm mb-2"> {product.name} </h3>
+                 <p className="text-gray-500 font-medium text-sm tracking-tighter"> ${product.price} </p>
+                 
             </div>
            </Link> 
         ))}
