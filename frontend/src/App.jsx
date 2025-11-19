3d80fc71d241b54c3f7a5ae2fd807e942f9b7bf9
aman <git>
2025-11-19 14:40:08 +0600

    CollectionPage/Done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,6 +6,8 @@ import { Toaster } from "sonner";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Profile from "./pages/Profile";
+import CollectionPage from "./pages/CollectionPage";
+import ProductDetails from "./components/Products/ProductDetails";
 
 const App = () => {
   return (
@@ -17,7 +19,10 @@ const App = () => {
           <Route path="login" element={<Login />} />
           <Route path="register" element={<Register />} />
           <Route path="profile" element={<Profile />} />
+          <Route path="collection/:collection" element={<CollectionPage />} />
+          <Route path="product/:id" element={<ProductDetails />} />
         </Route>
+
         <Route>{/* Admin Layout */}</Route>
       </Routes>
     </BrowserRouter>
